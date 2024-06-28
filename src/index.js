38a87d8d59cab1671fb329c43f7c9ec6e15f608f
Vu Nguyen <vu.nguyen@dmttek.com>
2024-06-28 16:00:08 +0700

    dist

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Footer, { EVENT_NAMES } from './components/Footer';
-import { getCurrentLanguageCode } from './components/handleLanguageChange';
-import messages from './i18n/index';
-
-export default Footer;
-export { messages, EVENT_NAMES, getCurrentLanguageCode };
